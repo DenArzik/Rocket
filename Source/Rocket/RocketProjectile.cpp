9e5a9f0ff5a18497c8ff73b89c54bc1c8e341780
DenArzik <denisthekwondist@gmail.com>
2017-08-05 20:09:51 +0300

    Update RocketProjectile.cpp

--- Source/Rocket/RocketProjectile.cpp
+++ Source/Rocket/RocketProjectile.cpp
@@ -1,43 +1,188 @@
-// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+// Fill out your copyright notice in the Description page of Project Settings.
 
-#include "RocketProjectile.h"
-#include "GameFramework/ProjectileMovementComponent.h"
-#include "Components/SphereComponent.h"
+#include "MyProjectile.h"
+#include "Engine.h"
+#include "Runtime/Engine/Classes/Components/PrimitiveComponent.h"
+#include "UObject/ConstructorHelpers.h"
 
-ARocketProjectile::ARocketProjectile() 
+
+// Sets default values
+AMyProjectile::AMyProjectile()
+	:FunctionScale(3.f)
+	, FirstStepVelocity(0.5f)
+	, SecondStepVelocity(5000.f)
+	, SplinePoints(5)
+	, TraceLength(1000.f)
+	, PassedInitialTrace(false)
+	, Target(nullptr)
+	, ExplosionRadius(1000.f)
+	, ExplosionForce(100000000.f)
 {
-	// Use a sphere as a simple collision representation
-	CollisionComp = CreateDefaultSubobject<USphereComponent>(TEXT("SphereComp"));
-	CollisionComp->InitSphereRadius(5.0f);
-	CollisionComp->BodyInstance.SetCollisionProfileName("Projectile");
-	CollisionComp->OnComponentHit.AddDynamic(this, &ARocketProjectile::OnHit);		// set up a notification for when this component hits something blocking
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
 
-	// Players can't walk on it
-	CollisionComp->SetWalkableSlopeOverride(FWalkableSlopeOverride(WalkableSlope_Unwalkable, 0.f));
-	CollisionComp->CanCharacterStepUpOn = ECB_No;
+	SceneComp = CreateDefaultSubobject<USceneComponent>(TEXT("SceneComp"));
+	RootComponent = SceneComp;
 
-	// Set as root component
-	RootComponent = CollisionComp;
+	Collision = CreateDefaultSubobject<UCapsuleComponent>(TEXT("Collision"));
+	Collision->OnComponentBeginOverlap.AddDynamic(this, &AMyProjectile::OnBeginOverlap);				//OnComponentHit.AddDynamic(this, &AMyProjectile::OnHit);
+	Collision->AttachToComponent(SceneComp, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
 
-	// Use a ProjectileMovementComponent to govern this projectile's movement
-	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileComp"));
-	ProjectileMovement->UpdatedComponent = CollisionComp;
-	ProjectileMovement->InitialSpeed = 3000.f;
-	ProjectileMovement->MaxSpeed = 3000.f;
-	ProjectileMovement->bRotationFollowsVelocity = true;
-	ProjectileMovement->bShouldBounce = true;
+	Projectile = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Projectile"));
+	Projectile->AttachToComponent(SceneComp, FAttachmentTransformRules::SnapToTargetNotIncludingScale);
 
-	// Die after 3 seconds by default
-	InitialLifeSpan = 3.0f;
+	Trace = CreateDefaultSubobject<USplineComponent>(TEXT("Trace"));
+	Trace->RemoveSplinePoint(1);
+	Trace->RemoveSplinePoint(0);
+
+	MovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("MovemetComponent"));
+	MovementComponent->bRotationFollowsVelocity = false;
+
+	Sound = CreateDefaultSubobject<USoundWave>(TEXT("Sound"));
+
+	ExplosionEffect = CreateDefaultSubobject<UParticleSystem>(TEXT("ExplosionEffect"));
+
+	Timeline = CreateDefaultSubobject<UTimelineComponent>(TEXT("Timeline"));
+	InterpFunction.BindUFunction(this, TEXT("TimelineFloatReturn"));
+
+	static ConstructorHelpers::FObjectFinder<UCurveFloat> curve(TEXT("/Game/Geometry/MyCurve"));
+	if (curve.Object) {
+		Curve = curve.Object;
+	}
+	InitialLifeSpan = 5.0f;
 }
 
-void ARocketProjectile::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
-{
-	// Only add impulse and destroy projectile if we hit a physics
-	if ((OtherActor != NULL) && (OtherActor != this) && (OtherComp != NULL) && OtherComp->IsSimulatingPhysics())
-	{
-		OtherComp->AddImpulseAtLocation(GetVelocity() * 100.0f, GetActorLocation());
 
-		Destroy();
+
+// Called when the game starts or when spawned
+void AMyProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FRotator temp = this->GetActorRotation();
+	temp.Pitch = 0.f;
+	this->SetActorRotation(temp);
+
+	startPosition = this->GetActorLocation();
+	startRotation = this->GetActorRotation();
+
+
+	Timeline->AddInterpFloat(Curve, InterpFunction, TEXT("FloatValue"));
+	Timeline->SetPlayRate(FirstStepVelocity);
+	Timeline->PlayFromStart();
+
+
+	Scalar = FunctionScale / TraceLength;
+	float x(0), z(0);
+	delta_x = (TraceLength / SplinePoints) * Scalar;
+
+	float X = startPosition.X;
+	float Y = startPosition.Y;
+	float Z = startPosition.Z;
+
+	for (int i(0); i < SplinePoints; i++)
+	{
+		Trace->AddSplinePoint(startRotation.RotateVector(FVector((x / Scalar), 0.f, (z / Scalar))), ESplineCoordinateSpace::World);
+		x += delta_x;
+		z = SplineFunction(x);
+	}
+
+	//Additional points to make spline smoother
+	Trace->AddSplinePoint(Trace->GetLocationAtSplinePoint(SplinePoints - 1, ESplineCoordinateSpace::World) + startRotation.RotateVector(FVector(TraceLength / 5, 0, TraceLength / 5)), ESplineCoordinateSpace::World);
+	Trace->AddSplinePoint(Trace->GetLocationAtSplinePoint(SplinePoints, ESplineCoordinateSpace::World) + startRotation.RotateVector(FVector(TraceLength / 5, 0, 0)), ESplineCoordinateSpace::World);
+
+	//It offsets for some reason at random distantion so i repeatedly set it back
+	Trace->SetWorldLocation(startPosition);
+}
+
+// Called every frame
+void AMyProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (PassedInitialTrace)
+	{
+		if (Target)
+		{
+			//Some kind of interpolation
+			//It is for smoother targeted flight
+			if (decreasingVelocity > 0)
+				decreasingVelocity -= SecondStepVelocity * DeltaTime;
+			MovementComponent->Velocity = startRotation.RotateVector(FVector(decreasingVelocity, 0.f, 0.f));
+
+
+			MovementComponent->HomingAccelerationMagnitude = SecondStepVelocity * 100;
+		}
+		else
+		{
+			//Just load it forward
+			MovementComponent->Velocity = startRotation.RotateVector(FVector(SecondStepVelocity, 0.f, 0.f));
+		}
+	}
+}
+
+
+// Called when hit
+void AMyProjectile::OnBeginOverlap(class UPrimitiveComponent* OverlappedComp, class AActor* OtherActor, class UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (!(OtherActor->GetClass()->IsChildOf(this->GetClass())))
+	{
+		TArray<UPrimitiveComponent*> objectsToAdjustImpulse;
+		UGameplayStatics::PlaySoundAtLocation(this, Sound, OtherActor->GetActorLocation(), 2);
+
+		for (TObjectIterator<UPrimitiveComponent> CompItr; CompItr; ++CompItr)
+		{
+			if (FVector::Distance(Collision->GetComponentLocation(), CompItr->GetComponentLocation()) <= ExplosionRadius)
+				objectsToAdjustImpulse.Add(*CompItr);
+		}
+
+		for (int i(0); i < objectsToAdjustImpulse.Num(); i++)
+			objectsToAdjustImpulse[i]->AddRadialForce(Collision->GetComponentLocation(), ExplosionRadius, ExplosionForce, RIF_Linear);
+
+		UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), ExplosionEffect, FTransform(Collision->GetComponentLocation()), true);
+		this->Destroy();
+	}
+}
+
+
+float AMyProjectile::SplineFunction(const float &x)
+{
+	return x*x;
+}
+
+
+void AMyProjectile::SetTarget(AActor *target)
+{
+	Target = target;
+	if (Target)
+	{
+		FVector MyPosition(this->GetActorLocation());
+		FVector TargetPosition(Target->GetActorLocation());
+
+		TraceLength = FVector::DistXY(MyPosition, TargetPosition) / 10;
+
+		MovementComponent->bIsHomingProjectile = true;
+		MovementComponent->HomingAccelerationMagnitude = 0.f;
+		MovementComponent->HomingTargetComponent = Target->GetRootComponent();
+		decreasingVelocity = SecondStepVelocity;
+	}
+}
+
+void AMyProjectile::TimelineFloatReturn(float val)
+{
+	//I took local position and rotation at spline by time
+	FVector vector = Trace->GetLocationAtTime(val, ESplineCoordinateSpace::World);
+	FRotator rotator = Trace->GetRotationAtTime(val, ESplineCoordinateSpace::Local);
+
+	//And set it to my projectile lile offset
+	SceneComp->SetWorldLocation(vector+FVector(10.f,10.f,0));
+	SceneComp->SetWorldRotation(rotator);
+
+	//If we went through spline
+	if (1 == val)
+	{
+		PassedInitialTrace = true;
+		Trace->DestroyComponent();
+		MovementComponent->bRotationFollowsVelocity = true;
 	}
 }
